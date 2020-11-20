54f2501469023313d93e5a947bdbbd45920b8cc0
Borna Ungar <36451381+Bungar95@users.noreply.github.com>
2020-11-20 19:57:24 +0100

    Initial commit

---
+++ app/src/main/java/hr/cnzd/dsi2021/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package hr.cnzd.dsi2021.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hr.cnzd.dsi2021.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
